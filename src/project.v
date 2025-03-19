3b27cf3dbbcced685533a04800cd41fc0dd392c3
Revanth A <122678184+revanth102002@users.noreply.github.com>
2025-03-19 12:46:39 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -21,7 +21,7 @@ module tt_um_fsm (
 	wire [1:0]west;
 
 	traffic u1(.clk(clk),.reset(rst_n),.north(north),.east(east),.south(south),.west(west));
-	assign uo_out={north,east,south,west}
+	assign uo_out={north,east,south,west};
 
   // All output pins must be assigned. If not used, assign to 0.
  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
