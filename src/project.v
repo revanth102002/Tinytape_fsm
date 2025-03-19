c261991e4d78aea887318e4891a592949c62a6bb
Revanth A <122678184+revanth102002@users.noreply.github.com>
2025-03-19 10:14:50 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,6 +16,8 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+
+
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
@@ -25,3 +27,176 @@ module tt_um_example (
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
+
+
+
+
+module traffic(
+    input  clk,
+    input reset,
+    output reg [1:0] north,
+    output reg [1:0] east,
+    output reg [1:0] south,
+    output reg [1:0] west
+);
+parameter yellow = 2'b01;
+    parameter green = 2'b10;
+    parameter red = 2'b00;
+    parameter RST = 4'b0000;
+    parameter s0 = 4'b0001;
+    parameter s1 = 4'b0010;
+    parameter s2 = 4'b0011;
+    parameter s3 = 4'b0100;
+    parameter s4 = 4'b0101;
+    parameter s5 = 4'b0110;
+    parameter s6 = 4'b0111;
+    parameter s7 = 4'b1000;
+	 reg [3:0] state;
+    reg [3:0] nextstate; 
+	wire [4:0]sec_timer;
+	timer sec_time(.clk(clk),.reset(reset),.sec_timer(sec_timer));
+	 always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            state <= RST;
+        end else begin
+                state <= nextstate;
+					  end
+    end
+
+  always @(state,sec_timer) begin
+  nextstate = state;
+        case (state)
+            RST  : begin
+                north = yellow;
+                east = yellow;
+                south = yellow;
+                west = yellow;
+		          if(sec_timer==5'd0)
+							nextstate = s0;
+							else 
+								nextstate=nextstate;
+           end
+            s0: begin
+                north = green;
+                east = red;
+                south = red;
+                west = red;
+					 if(sec_timer==5'd5)
+						nextstate = s1;
+							else 
+								nextstate=nextstate;
+						end
+            s1: begin
+                north = yellow;
+                east = yellow;
+                south = red;
+					 west = red;
+				    if(sec_timer==5'd6)
+						nextstate = s2;
+						else 
+						nextstate=nextstate;
+                end
+            s2: begin
+                north = red;
+                east = green;
+                south = red;
+					 west = red;
+				if(sec_timer==5'd11)
+					nextstate = s3;
+					else 
+					nextstate =nextstate;
+					end
+            s3: begin
+                north = red;
+                east = yellow;
+                south = yellow;
+					 west = red;
+					if(sec_timer==5'd12)
+						nextstate = s4;
+						else 
+						nextstate=nextstate;
+					end
+            s4: begin
+                north = red;
+                east = red;
+                south = green;
+					 west = red;
+					if(sec_timer==5'd17)
+						nextstate = s5;
+						else 
+						nextstate=nextstate;
+					end
+            s5: begin
+                north = red;
+                east = red;
+                south = yellow;
+					 west = yellow;
+				    if(sec_timer==5'd18)
+						nextstate = s6;
+					else 
+						nextstate=nextstate;
+					end
+            s6: begin
+                north = red;
+                east = red;
+                south = red;
+					 west = green;
+					if(sec_timer==5'd23)
+						nextstate = s7;
+						else 
+						nextstate=nextstate;
+					end
+            s7: begin
+                north = yellow;
+                east = red;
+                south = red;
+					 west = yellow;
+					if(sec_timer==5'd24)
+						nextstate = s0;
+					else 
+						nextstate =nextstate;
+					end
+            default: begin
+                north = red;
+                east = red;
+                south= red;
+					 west = red;
+				if(sec_timer==5'd0)
+					nextstate = RST;
+				else 
+					nextstate =nextstate;
+            end
+        endcase
+    end
+endmodule
+
+
+
+module timer (
+    input clk,
+    input reset,
+    output reg [4:0] sec_timer
+);
+
+    localparam FREQ = 50*1000*1000; // Adjust this for actual usage, here it's for simulation
+
+    reg [25:0] count;
+
+    always @(posedge clk) begin
+	 if (reset) begin
+            count <= 26'd0;
+            sec_timer <= 5'd0;
+        end else begin
+            if (count == FREQ-1 ) begin
+                count <= 26'd0;
+                if (sec_timer == 5'd24) // Max value for sec_timer to reset at 25
+                    sec_timer <= 5'd0;
+                else
+                    sec_timer <= sec_timer + 1'b1;
+						  end else begin
+                count <= count + 1'b1;
+            end
+        end
+    end
+endmodule
+
